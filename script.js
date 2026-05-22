a24aa40afd875ba99e172467058a68a73f518b0b
moataz1411 <mezoahmed1411@gmail.com>
2026-05-22 23:31:44 +0300

    rename pause button to stop and implement timer functionality

---
+++ script.js
@@ -0,0 +1,40 @@
+const start=document.getElementById("start");
+const stop=document.getElementById("stop");
+const reset=document.getElementById("reset");
+const timer=document.getElementById("timer");
+
+let timeLeft=1500;
+let interval;
+
+const updateTimer = () => {
+    const minutes=Math.floor(timeLeft/60);
+    const seconds=timeLeft%60;
+    timer.textContent=`${minutes.toString().padStart(2,'0')}:${seconds.toString().padStart(2,'0')}`;
+};
+const startTimer = () => {
+    if (interval) return;
+    interval=setInterval(() => {
+    timeLeft--;
+    updateTimer();
+    if(timeLeft<=0){
+        clearInterval(interval);
+        alert("Congratulations!, Time is up.")
+        interval=null;
+        timeLeft=1500;
+        updateTimer();
+        }
+    }, 1000);
+};
+const stopTimer = () => {
+    clearInterval(interval);
+    interval=null;
+};
+const resetTimer=() => {
+    clearInterval(interval);
+    interval=null;
+    timeLeft=1500;
+    updateTimer();
+}
+start.addEventListener("click", startTimer);
+stop.addEventListener("click", stopTimer);
+reset.addEventListener("click",resetTimer);
